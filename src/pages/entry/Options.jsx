d5c9b1fd80037b91ba94aa1592f5baa587edc254
PDFAtauchi <floresatauchi@gmail.com>
2023-09-22 17:14:35 -0500

    chore: toppings option implementation

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ScoopOption from "pages/entry/ScoopOption";
+import ToppingOption from "pages/entry/ToppingOption";
 import Row from "react-bootstrap/Row";
 
 export default function Options({ optionType }) {
@@ -15,8 +16,7 @@ export default function Options({ optionType }) {
       });
   }, [optionType]);
 
-  // TODO: replace null with toppingOption
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
