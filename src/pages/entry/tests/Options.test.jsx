5ddb24b3d99d5bdcfb2753affcda9b78494063de
PDFAtauchi <floresatauchi@gmail.com>
2023-09-22 16:44:22 -0500

    chore: scoop images from server implementation

---
+++ src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Options from "pages/entry/Options";
+
+test("displays images for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  const altTexts = scoopImages.map((element) => element.alt);
+  expect(altTexts).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
