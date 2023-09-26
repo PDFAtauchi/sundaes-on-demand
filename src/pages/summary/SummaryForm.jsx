b219875e675943e2a2bab4850a4c512e55badb76
PDFAtauchi <floresatauchi@gmail.com>
2023-09-25 19:12:13 -0500

    feat: order phase implementation

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -3,8 +3,14 @@ import { Form, Button } from "react-bootstrap";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Popover from "react-bootstrap/Popover";
 
-export default function SummaryForm() {
+export default function SummaryForm({ setOrderPhase }) {
   const [tcChecked, setTcChecked] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    setOrderPhase("completed");
+  }
 
   const popover = (
     <Popover id="popover-basic">
@@ -23,7 +29,7 @@ export default function SummaryForm() {
     </span>
   );
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="terms-and-conditions">
         <Form.Check
           type="checkbox"
---
+++ src/tests/orderPhase.test.jsx
@@ -0,0 +1,89 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import App from "App";
+
+test("order phases for happy path", async () => {
+  const user = userEvent.setup();
+  // render app
+  render(<App />);
+
+  // add ice cream scoops and toppings
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+
+  const cherriesInput = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesInput);
+
+  // find and click order summary button
+  const OrderSummaryButton = screen.getByRole("button", {
+    name: /order sundae/i,
+  });
+  await user.click(OrderSummaryButton);
+
+  // check summmary subtotals
+  const summaryHeading = screen.getByRole("heading", { name: "Order Summary" });
+  expect(summaryHeading).toBeInTheDocument();
+
+  const scoopsHeading = screen.getByRole("heading", { name: "Scoops: $6.00" });
+  expect(scoopsHeading).toBeInTheDocument();
+
+  const toppingsHeading = screen.getByRole("heading", {
+    name: "Toppings: $1.50",
+  });
+  expect(toppingsHeading).toBeInTheDocument();
+
+  // check summary option items
+  expect(screen.getByText("1 Vanilla")).toBeInTheDocument();
+  expect(screen.getByText("2 Chocolate")).toBeInTheDocument();
+  expect(screen.getByText("1 Cherries")).toBeInTheDocument();
+
+  // accept terms and conditions and click button to confirm order
+  const tcCheckbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  await user.click(tcCheckbox);
+
+  const confirmOrderButton = screen.getByRole("button", {
+    name: /confirm Order/i,
+  });
+  await user.click(confirmOrderButton);
+
+  // expect loading to show
+  const loading = screen.getByText(/loading/i);
+  expect(loading).toBeInTheDocument();
+
+  // thanks page
+  const thankYouHeader = await screen.findByRole("heading", {
+    name: /Thank you!/i,
+  });
+  expect(thankYouHeader).toBeInTheDocument();
+
+  // expect that loading has disappeared
+  const notLoading = screen.queryByText("loading");
+  expect(notLoading).not.toBeInTheDocument();
+
+  // find and click new order button
+  const newOrderButton = screen.getByRole("button", {
+    name: /Create new order/i,
+  });
+  await user.click(newOrderButton);
+
+  // check that scoops and toppings have been reset
+  const scoopsTotal = await screen.findByText("Scoops total: $0.00");
+  expect(scoopsTotal).toBeInTheDocument();
+
+  const toppingsTotal = await screen.findByText("Toppings total: $0.00");
+  expect(toppingsTotal).toBeInTheDocument();
+});
