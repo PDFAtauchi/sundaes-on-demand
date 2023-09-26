46f78531342b9ca0cf4b21cf8fe413904529a050
PDFAtauchi <floresatauchi@gmail.com>
2023-09-25 20:55:16 -0500

    feat: disable order button if there is no scoops ordered

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -4,9 +4,15 @@ import OrderEntry from "pages/entry/OrderEntry";
 
 import { rest } from "msw";
 import { server } from "mocks/server";
+import userEvent from "@testing-library/user-event";
+
+beforeEach(() => {
+  // Reset the MSW server handlers before each test
+  server.resetHandlers();
+});
 
 test("handles error for scoops and toppings routes", async () => {
-  server.resetHandlers(
+  server.use(
     rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
       return res(ctx.status(500));
     }),
@@ -20,4 +26,23 @@ test("handles error for scoops and toppings routes", async () => {
     const alerts = await screen.findAllByRole("alert");
     expect(alerts).toHaveLength(2);
   });
+});
+
+test("disable order button if there is no scoops ordered", async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  const orderButton = screen.getByRole("button", { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
 });
