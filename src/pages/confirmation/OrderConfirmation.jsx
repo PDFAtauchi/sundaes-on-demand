5eb14a7bf82ef903ec76cc427923f95df3561374
PDFAtauchi <floresatauchi@gmail.com>
2023-09-25 21:28:15 -0500

    feat: alert when error on submit order

--- src/pages/confirmation/OrderConfirmation.jsx
+++ src/pages/confirmation/OrderConfirmation.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import { useOrderDetails } from "contexts/OrderDetails";
+import AlertBanner from "pages/common/AlertBanner";
 
 export default function OrderConfirmation({ setOrderPhase }) {
   const { resetOrder } = useOrderDetails();
   const [orderNumber, setOrderNumber] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
@@ -13,8 +15,23 @@ export default function OrderConfirmation({ setOrderPhase }) {
       .then((response) => {
         setOrderNumber(response.data.orderNumber);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        setError(true);
+      });
   }, []);
+
+  const newOrderButton = (
+    <Button onClick={handleClick}> Create new order</Button>
+  );
+
+  if (error) {
+    return (
+      <>
+        <AlertBanner message={null} variant={null} />
+        {newOrderButton}
+      </>
+    );
+  }
 
   function handleClick() {
     resetOrder();
@@ -29,7 +46,7 @@ export default function OrderConfirmation({ setOrderPhase }) {
         <p style={{ fontSize: "25%" }}>
           as per our terms and conditions, nothing will happen now
         </p>
-        <Button onClick={handleClick}>Create new order</Button>
+        {newOrderButton}
       </div>
     );
   } else {
